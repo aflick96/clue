ff556ad8e9b414e013cca47638e8b367233e5cbc
Andrew Flick <andrew.flick@jhuapl.edu>
2024-02-06 19:34:27 -0500

    host attempt

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const http = require('http');
 const app = express();
+const path = require('path');
 const server = http.createServer(app);
 const cors = require('cors');
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000" || "https://YOURAPPNAME.adaptable.app",
     methods: ["GET", "POST"]
   }
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use(cors());
