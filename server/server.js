4f15faeb1f53d9be67278086243b41d1e60d517d
Andrew Flick <andrew.flick@jhuapl.edu>
2024-02-06 20:38:54 -0500

    try again

--- server/server.js
+++ server/server.js
@@ -12,11 +12,11 @@ const io = new Server(server, {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.use(cors());
 
